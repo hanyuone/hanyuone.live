bf752d4a9736467670d4922d8e867dbb7a086675
Hanyuan Li <work@hanyuone.liv>
2024-07-29 17:54:29 +1000

    feat(callout): add callout component

--- website/src/components/blog_post/callout.rs
+++ website/src/components/blog_post/callout.rs
@@ -1,12 +1,26 @@
-use yew::{function_component, html, Children, Html, Properties};
+use yew::{classes, function_component, html, Children, Html, Properties};
+use yew_icons::{Icon, IconId};
 
 #[derive(Properties, PartialEq)]
 struct CalloutProps {
+    colour: String,
+    icon: IconId,
+    title: String,
     #[prop_or_default]
-    pub children: Children,
+    children: Children,
 }
 
 #[function_component(Callout)]
 fn callout(props: &CalloutProps) -> Html {
-    html! {}
+    html! {
+        <div class={props.colour.clone()}>
+            <div class="flex flex-row">
+                <Icon icon_id={props.icon} />
+                <p class="font-bold">{props.title.clone()}</p>
+            </div>
+            <article class="prose dark:prose-invert">
+                {props.children.clone()}
+            </article>
+        </div>
+    }
 }
