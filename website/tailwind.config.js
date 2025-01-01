0168a26e684daf65cb296b97d78ec2fbcfa37559
Hanyuan Li <work@hanyuone.live>
2025-01-01 18:11:30 +0100

    style: ensure images in mergecells don't cause text to overflow in next line

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -34,16 +34,21 @@ module.exports = {
             typography: {
                 DEFAULT: {
                     css: {
+                        // Blockquotes
                         "blockquote p:first-of-type::before": false,
                         "blockquote p:first-of-type::after": false,
                         blockquote: {
                             fontWeight: false,
                             fontStyle: false,
                         },
+                        // Tables
                         "thead th:first-child": false,
                         "thead th:last-child": false,
                         "tbody td:first-child, tfoot td:first-child": false,
                         "tbody td:last-child, tfoot td:last-child": false,
+                        td: {
+                            verticalAlign: "top",
+                        },
                     },
                 },
             },
