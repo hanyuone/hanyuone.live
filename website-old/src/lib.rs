626c18866f3b85b20ac97b2aba644ad953063739
Hanyuan Li <work@hanyuone.live>
2025-11-28 13:00:33 +1100

    feat: initial commit for leptos rewrite

---
+++ website-old/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod app;
+pub mod components;
+pub mod context;
+pub mod pages;
+mod render;
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,44 +2,9 @@ module.exports = {
     content: [
         "./src/**/*.rs",
         "./index.html",
-        "./src/**/*.html",
-        "./src/**/*.css",
     ],
     theme: {
         extend: {
-            animation: {
-                blink: "blink 1s infinite",
-            },
-            colors: {
-                black: "#32302F",
-                gray: "#504945",
-                red: "#FB4934",
-                orange: "#FE8019",
-                yellow: "#FABD2F",
-                green: "#B8BB26",
-                cyan: "#8EC07C",
-                blue: "#83A598",
-                purple: "#D3869B",
-            },
-            height: {
-                "fullmain": "calc(100vh - 7rem - 2px)",
-                // Screen height minus header/footer and padding
-                "main": "calc(100vh - 17rem - 2px)",
-            },
-            keyframes: {
-                blink: {
-                    "0%, 100%": {
-                        "background-color": "transparent",
-                    },
-                    "50%": {
-                        "background-color": "white",
-                    },
-                },
-            },
-            margin: {
-                // Screen height minus header/footer and padding
-                "main": "calc(100vh - 17rem - 2px)",
-            },
             typography: {
                 DEFAULT: {
                     css: {
@@ -63,11 +28,4 @@ module.exports = {
             },
         },
     },
-    variants: {},
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-    safelist: [
-        "hover:bg-purple",
-    ],
 };
