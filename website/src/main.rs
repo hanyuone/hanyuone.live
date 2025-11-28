918e47bb94ed278a5c54ba8f35af89874a7456a4
Hanyuan Li <work@hanyuone.live>
2025-11-28 16:54:14 +1100

    feat: change to cargo leptos serve

--- website/src/main.rs
+++ website/src/main.rs
@@ -2,11 +2,10 @@
 #[tokio::main]
 async fn main() {
     use leptos::prelude::*;
-    use leptos_axum::{generate_route_list_with_ssg, LeptosRoutes};
+    use leptos_axum::{generate_route_list_with_ssg};
     use website::app::*;
 
     let conf = get_configuration(None).unwrap();
-    let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
 
     // Generate the list of routes in your Leptos App
@@ -17,9 +16,13 @@ async fn main() {
 
     static_routes.generate(&leptos_options).await;
 
+    #[cfg(debug_assertions)]
     {
         use axum::Router;
         use leptos::logging::log;
+        use leptos_axum::LeptosRoutes;
+
+        let addr = leptos_options.site_addr;
 
         let app = Router::new()
             .leptos_routes(&leptos_options, routes, {
