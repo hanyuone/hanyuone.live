5912413df2d5502027aec80be324d7751cf635a2
Hanyuan Li <work@hanyuone.live>
2026-03-04 21:24:05 +1100

    feat: add basic comments component

--- website/src/components/blog_post/comments.rs
+++ website/src/components/blog_post/comments.rs
@@ -1,10 +1,10 @@
 use icondata as i;
 use leptos::prelude::*;
 use leptos_icons::Icon;
-use leptos_router::hooks::use_location;
+use leptos_use::use_document;
 use serde::{Deserialize, Serialize};
 
-use crate::{COMMENTS_URL, WEBSITE_URL};
+use crate::COMMENTS_URL;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct User {
@@ -15,10 +15,15 @@ pub struct User {
 
 #[island]
 fn OAuth() -> impl IntoView {
-    let route = use_location().pathname.get();
+    let document = use_document();
 
-    let return_url = format!("{WEBSITE_URL}{route}");
-    let login_url = format!("{COMMENTS_URL}/auth/login?return_url={return_url}");
+    let login_url = match document.url() {
+        Some(url) => Some(format!(
+            "{COMMENTS_URL}/auth/login?return_url={}",
+            url.unwrap()
+        )),
+        None => None,
+    };
 
     view! {
         <div class="flex flex-col content-center">
@@ -34,9 +39,9 @@ fn OAuth() -> impl IntoView {
     }
 }
 
-#[component]
+#[island]
 pub fn Comments() -> impl IntoView {
-    let auth_state = OnceResource::new(get_profile());
+    let auth_state = LocalResource::new(get_profile);
 
     view! {
         <Suspense fallback=move || view! { <div>{"Loading..."}</div> }>
@@ -45,7 +50,9 @@ pub fn Comments() -> impl IntoView {
 
                 match auth_state {
                     Ok(ref user) => {
-                        view! { <p>{format!("Signed in as user {}", user.username)}</p> }.into_any()
+                        view! {
+                            <p>{format!("Signed in as user {}", user.username)}</p>
+                        }.into_any()
                     }
                     Err(_) => view! { <OAuth /> }.into_any(),
                 }
@@ -54,14 +61,25 @@ pub fn Comments() -> impl IntoView {
     }
 }
 
-#[server]
-async fn get_profile() -> Result<User, ServerFnError> {
-    let url = format!("{COMMENTS_URL}/profile");
+async fn get_profile() -> Result<User, Error> {
+    #[cfg(target_arch = "wasm32")]
+    {
+        use reqwest::Client;
 
-    let user = reqwest::get(url)
-        .await?
-        .json::<User>()
-        .await?;
+        let url = format!("{COMMENTS_URL}/profile");
 
-    Ok(user)
+        let client = Client::new();
+        let user = client
+            .get(url)
+            .fetch_credentials_include()
+            .send()
+            .await?
+            .json::<User>()
+            .await?;
+
+        Ok(user)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    Err("Only supported on WASM mode".into())
 }
--- website/src/lib.rs
+++ website/src/lib.rs
@@ -6,12 +6,12 @@ pub mod renderer;
 
 const ROOT: Option<&'static str> = option_env!("ROOT");
 
-const WEBSITE_URL: &'static str = env!("WEBSITE_URL");
 const COMMENTS_URL: &'static str = env!("COMMENTS_URL");
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
+    use app::*;
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_islands();
 }
