ac25ff80c532eb3701e6aa3e0424d9bb14fc42b6
Hanyuan Li <work@hanyuone.live>
2026-03-14 16:30:42 +1100

    feat: add minimal comments widget

--- website/src/pages/blog_post.rs
+++ website/src/pages/blog_post.rs
@@ -9,13 +9,15 @@ use markdown::{
 };
 
 use crate::{
-    components::{blog::to_read_time, blog_post::comments::Comments},
+    components::{blog::to_read_time, blog_post::comments::CommentToggle},
     context::BlogContext,
 };
 
+pub type BlogSlug = String;
+
 #[derive(Params, PartialEq)]
 pub struct BlogPostParams {
-    slug: Option<String>,
+    slug: Option<BlogSlug>,
 }
 
 #[component]
@@ -28,6 +30,8 @@ pub fn BlogPostPage() -> impl IntoView {
         .and_then(|params| params.slug.clone())
         .unwrap_or_default();
 
+    provide_context(slug.clone());
+
     let context = use_context::<BlogContext>().unwrap();
     let slug_id = BlogId::from_str(&slug).map_err(ServerFnError::new).unwrap();
 
@@ -36,41 +40,44 @@ pub fn BlogPostPage() -> impl IntoView {
         post_translate,
     } = context.get(&slug_id).unwrap().clone();
 
-    let post_contents = Resource::new(move || slug.clone(), get_blog_post_ron);
+    let post_contents = {
+        let slug = slug.clone();
+        Resource::new(move || slug.clone(), get_blog_post_ron)
+    };
 
     view! {
         <Title text={front_matter.title.clone()} />
-        <div class="flex flex-col p-4 content-center text-center border-b-[1px]">
-            <h2 class="font-bold text-2xl underline">{front_matter.title}</h2>
-            <p>
-                <span class="text-gray-500">{front_matter.publish_date.format("%d %b %Y").to_string()}</span>
-                <span class="px-1 text-white">{"·"}</span>
-                <span class="text-gray-500">{to_read_time(post_translate.words)}</span>
-            </p>
-        </div>
-        <Suspense fallback=move || view! { <p>"Loading post..."</p> }>
-            {move || Suspend::new(async move {
-                let renderer = crate::renderer::Renderer::new();
+        <div class="flex flex-col items-center">
+            <div class="flex flex-col p-4 text-center border-b-[1px]">
+                <h2 class="font-bold text-2xl underline">{front_matter.title}</h2>
+                <p>
+                    <span class="text-gray-500">{front_matter.publish_date.format("%d %b %Y").to_string()}</span>
+                    <span class="px-1 text-white">{"·"}</span>
+                    <span class="text-gray-500">{to_read_time(post_translate.words)}</span>
+                </p>
+            </div>
+            <div class="flex flex-col py-4 items-center">
+                <Suspense fallback=move || view! { <p>"Loading post..."</p> }>
+                    {move || Suspend::new(async move {
+                        let renderer = crate::renderer::Renderer::new();
 
-                let post_contents = post_contents.await;
-                let rendered_views = match post_contents {
-                    Ok(post_contents) => {
-                        let post_nodes = ron::from_str::<Vec<RenderNode>>(&post_contents)
-                            .unwrap_or_default();
-                        Ok(renderer.run(post_nodes))
-                    },
-                    Err(e) => Err(e),
-                };
+                        let post_contents = post_contents.await;
+                        let rendered_views = match post_contents {
+                            Ok(post_contents) => {
+                                let post_nodes = ron::from_str::<Vec<RenderNode>>(&post_contents)
+                                    .unwrap_or_default();
+                                Ok(renderer.run(post_nodes))
+                            },
+                            Err(e) => Err(e),
+                        };
 
-                view! {
-                    <div class="flex flex-col py-4 items-center">
-                        {rendered_views}
-                    </div>
-                }
-            })}
-        </Suspense>
-        <div class="flex flex-col py-4 items-center">
-            <Comments />
+                        rendered_views
+                    })}
+                </Suspense>
+            </div>
+            <div class="max-w-[65ch] w-full">
+                <CommentToggle />
+            </div>
         </div>
     }
 }
