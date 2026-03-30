36c34b813742164b563afecccd4b0c471ba680f2
Hanyuan Li <work@hanyuone.live>
2026-03-30 18:05:58 +1100

    fix: add base, use path-relative URLs

--- website/src/components/blog/tag.rs
+++ website/src/components/blog/tag.rs
@@ -1,18 +1,16 @@
 use leptos::prelude::*;
+use leptos_router::components::A;
 use markdown::structs::tag::TagId;
-
-use crate::ROOT;
 
 #[component]
 pub fn Tag(name: String, colour: String) -> impl IntoView {
-    let root = ROOT.unwrap_or("");
-    let href = format!("{root}/tag/{}", name.parse::<TagId>().unwrap());
+    let href = format!("tag/{}", name.parse::<TagId>().unwrap());
 
     view! {
-        <a href=href class="inline-flex">
+        <A href=href.clone() {..} class="inline-flex">
             <div class={format!("rounded-sm mr-1 px-1 transition bg-{colour}/50 hover:bg-{colour}")}>
                 {name.clone()}
             </div>
-        </a>
+        </A>
     }
 }
